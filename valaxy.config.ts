8076d2f3d3270b60f1e9d29a681b1a2ccce56df4
MuziGeek <572428344@qq.com>
2024-11-30 16:01:45 +0800

    添加评论系统

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,5 +1,6 @@
 import type { UserThemeConfig } from 'valaxy-theme-yun'
 import { defineValaxyConfig } from 'valaxy'
+import { addonWaline } from 'valaxy-addon-waline'
 
 // add icons what you will need
 const safelist = [
@@ -43,6 +44,19 @@ export default defineValaxyConfig<UserThemeConfig>({
       },
     },
   },
+    siteConfig: {
+    // 启用评论
+    comment: {
+      enable: true
+    },
+  },
+  // 设置 valaxy-addon-waline 配置项
+  addons: [
+    addonWaline({
+      // Waline 配置项，参考 https://waline.js.org/reference/client/props.html
+      serverURL: 'https://blog.easymuzi.cn',
+    }),
+  ],
 
   unocss: { safelist },
 })
