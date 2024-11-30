49ead94aaba2c463c3497cbc4d943a8e35901926
MuziGeek <572428344@qq.com>
2024-11-30 13:17:23 +0800

    更新url

--- site.config.ts
+++ site.config.ts
@@ -1,7 +1,7 @@
 import { defineSiteConfig } from 'valaxy'
 
 export default defineSiteConfig({
-  url: 'https://valaxy.site/',
+  url: 'https://blog.easymuzi.cn',
   lang: 'zh-CN',
   title: 'Valaxy Theme Yun',
   author: {
