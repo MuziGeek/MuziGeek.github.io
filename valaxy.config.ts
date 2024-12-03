0b03d6b4c1468a4c745f2f864a521301d805d064
Muzi <572428344@qq.com>
2024-12-03 18:07:14 +0800

    添加音乐播放插件及访问次数

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -3,6 +3,9 @@ import { defineValaxyConfig } from 'valaxy'
 import { addonWaline } from 'valaxy-addon-waline'
 import { addonComponents } from 'valaxy-addon-components'
 import { addonAlgolia } from 'valaxy-addon-algolia'
+import { addonMeting } from 'valaxy-addon-meting'
+import { addonLightGallery } from 'valaxy-addon-lightgallery'
+
 
 
 // add icons what you will need
@@ -14,6 +17,12 @@ export default defineValaxyConfig<UserThemeConfig>({
   // site config see site.config.ts
 
   theme: 'yun',
+
+  themeConfig: {
+    colors: {
+      primary: 'black',
+    },
+  },
 
   modules: {
     rss: {
@@ -42,7 +51,17 @@ export default defineValaxyConfig<UserThemeConfig>({
       apiKey: '0493de45812d185d21c24c043254a5f9',
       indexName: 'my-valaxy-blog',
     }),
+    addonMeting({
+      global: true,
+      /** @see https://github.com/metowolf/MetingJS */
+      props: {
+        id: '3317014130',
+        server: 'netease',
+        type: 'song',
+      },
+    }),
     addonComponents(),
+    addonLightGallery(),
     addonWaline({
       // Waline 配置项，参考 https://waline.js.org/reference/client/props.html
       serverURL: 'https://waline.easymuzi.cn',
